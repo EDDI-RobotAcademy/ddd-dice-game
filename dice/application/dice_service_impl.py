19b47b7a30fd1abaa0c7d7ce5a445cd71803129a
Sanghoon Lee <gcccompil3r@gmail.com>
2026-02-03 21:59:09 +0900

    feat: [이상훈] 미인증 사용자의 주사위 추가 [DICE-3]

--- dice/application/dice_service_impl.py
+++ dice/application/dice_service_impl.py
@@ -1,12 +1,19 @@
 from dice.application.dice_service import DiceService
 from dice.domain.dice import Dice
 
-
 class DiceServiceImpl(DiceService):
-    """DiceService 구현체"""
+    __instance: DiceService | None = None
 
-    def __init__(self):
-        self.dice = Dice()
+    def __new__(cls):
+        if cls.__instance is None:
+            cls.__instance = super().__new__(cls)
 
-    def roll_dice(self) -> int:
-        return self.dice.roll()
+            cls.__instance.dice_list: list[Dice] = [Dice()]
+
+        return cls.__instance
+
+    def add_dice(self) -> None:
+        self.dice_list.append(Dice())
+
+    def roll_all(self) -> list[int]:
+        return [dice.roll() for dice in self.dice_list]
--- dice/infrastructure/api/dice_controller.py
+++ dice/infrastructure/api/dice_controller.py
@@ -4,7 +4,12 @@ from fastapi import APIRouter
 dice_router = APIRouter()
 dice_service: DiceServiceImpl = DiceServiceImpl()  # 인터페이스 기반으로 타입 지정 가능
 
+@dice_router.get("/add")
+def add_dice():
+    dice_service.add_dice()
+    return {"message": "Dice added"}
+
 @dice_router.get("/roll")
 def roll_dice():
-    result = dice_service.roll_dice()
+    result = dice_service.roll_all()
     return {"result": result}
